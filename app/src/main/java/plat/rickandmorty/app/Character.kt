b9687cf8be4a25609d7ec53142783b2c6d8a1836
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-09-25 10:42:28 -0600

    fix: missing files for V1 of lab 7

---
+++ app/src/main/java/plat/rickandmorty/app/Character.kt
@@ -0,0 +1,10 @@
+package plat.rickandmorty.app
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val gender: String,
+    val image: String
+)
